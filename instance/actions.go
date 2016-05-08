8603d240dc2debbfd5918ef1181459b79d8f2b0f
wudi <wudi@51idc.com>
2016-05-08 19:34:50 +0800

    Fix invalid fields type

--- instance/actions.go
+++ instance/actions.go
@@ -142,7 +142,7 @@ type ResetInstancesRequest struct {
 	LoginMode    qingcloud.String
 	LoginKeypair qingcloud.String
 	LoginPasswd  qingcloud.String
-	NeedNewsid   qingcloud.String
+	NeedNewsid   qingcloud.Integer
 }
 type ResetInstancesResponse qingcloud.CommonResponse
 
@@ -157,7 +157,7 @@ func (c *INSTANCE) ResetInstances(params ResetInstancesRequest) (ResetInstancesR
 type ResizeInstancesRequest struct {
 	InstancesN   qingcloud.NumberedString
 	InstanceType qingcloud.String
-	Cpu          qingcloud.String
+	Cpu          qingcloud.Integer
 	Memory       qingcloud.String
 	Zone         qingcloud.String
 }
